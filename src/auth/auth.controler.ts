8ad610801745dd4c86ef73e599feaf890c7d8b75
dmitry.pashkurov <dmitry.pahkurov@mail.ru>
2024-05-15 21:49:54 +0300

    complete authorization logic

--- src/auth/auth.controler.ts
+++ src/auth/auth.controler.ts
@@ -1,10 +1,19 @@
-import { Body, Controller, ParseIntPipe, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
 import { AuthDto } from 'src/network/dto';
 import { AuthService } from './auth.service';
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @HttpCode(HttpStatus.OK)
   @Post('login')
   private login(@Body() dto: AuthDto) {
     return this.authService.login(dto);
